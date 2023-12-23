f70a0f7232b69a74be681645091724fd6f7f57d5
Alexwing <alexwing@gmail.com>
2023-12-23 11:51:59 +0100

    Fix reactDom deprecated

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './config/routes';
+import React from 'react';
 
-ReactDom.render(
-    App(),
-    document.getElementById('app')
-);
+const root = document.getElementById('app');
+if (root !== null) {
+  createRoot(root).render(<App />);
+}
