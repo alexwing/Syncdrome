de4f7d4a87ccee11ee8e946534b5d85dec60c523
Alexwing <alexwing@gmail.com>
2024-12-29 21:25:43 +0100

    create service.exe and launch on tauri app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
+use tauri_plugin_shell::ShellExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            // Obtiene el handle de la aplicación
+            let handle = app.handle();
+            
+            // Configura el comando de "sidecar" llamado "service"
+            let sidecar_command = handle.shell().sidecar("service").unwrap();
+            
+            // Ejecuta el comando de "sidecar"
+            sidecar_command.spawn().expect("Failed to spawn sidecar");
+            
+            // Indica que la configuración se completó correctamente
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
